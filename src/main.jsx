f721786d6bcaf13271fb925560ce616c06b7d026
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-03-24 16:20:33 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+import Login from './pages/login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
