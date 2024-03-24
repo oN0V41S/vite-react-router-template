bae62838ce92bd967ea49015441218cefcfd4a5f
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-03-24 16:42:30 +0000

    Add error page and root layou

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+
+// Importando Componentes para criar roteador
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+// Importando Páginas localizadas na pasta ./pages
 import Home from './pages/home';
 import Login from './pages/login';
 
+// Importando página de Erro
+import ErrorPage from './error-page';
+
+// Criando variável de rotas
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
@@ -22,5 +31,8 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <-- Iniciando o roteador com as configurações da rota -->
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+    
